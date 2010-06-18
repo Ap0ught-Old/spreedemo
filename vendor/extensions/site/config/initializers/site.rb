23866e869dc6e6a005852fa737907c7efbe425e0
Steph Powell <steph@endpoint.com>
2010-06-18 15:30:27 -0600

    More design work.

--- vendor/extensions/site/config/initializers/site.rb
+++ vendor/extensions/site/config/initializers/site.rb
@@ -1,4 +1,5 @@
 if Preference.table_exists?
   Spree::Config.set(:stylesheets => 'screen,spreedemo')
   Spree::Config.set(:allow_ssl_in_production => false)
+  Spree::Config.set(:logo => '/images/logo.png')
 end

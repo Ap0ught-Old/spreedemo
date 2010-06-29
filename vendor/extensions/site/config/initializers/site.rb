88643a1908e3af2b7a4fbb9b3f88c6b9080b4c45
Steph Powell <steph@endpoint.com>
2010-06-29 17:23:38 -0600

    More sample data information. Some styling.

--- vendor/extensions/site/config/initializers/site.rb
+++ vendor/extensions/site/config/initializers/site.rb
@@ -2,4 +2,5 @@ if Preference.table_exists?
   Spree::Config.set(:stylesheets => 'spreedemo')
   Spree::Config.set(:allow_ssl_in_production => false)
   Spree::Config.set(:logo => '/images/logo.png')
+  Spree::Config.set(:products_per_page => 12)
 end
